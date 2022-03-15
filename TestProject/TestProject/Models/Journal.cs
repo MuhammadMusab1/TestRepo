1073f1b581b59330481b5af452cc78af5e04e316
musab <yaqoobmusab03@gmail.com>
2022-03-15 15:54:42 -0500

    Add the Journal Class and its relationship to the User

---
+++ TestProject/TestProject/Models/Journal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestProject.Models
+{
+    public class Journal
+    {
+        [Key]
+        public int JournalNumber { get; set; }
+        public string Entry { get; set; }
+        public DateTime DateWritten { get; set; }
+        public int UserNumber { get; set; }
+        [ForeignKey("UserNumber")]
+        public User User { get; set; }
+
+    }
+}
